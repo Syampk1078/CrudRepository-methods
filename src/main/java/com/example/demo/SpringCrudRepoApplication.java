beafde245fed77fae284b6036d6a1b11534aeec6
KANKANALA SYAM PRASAD <119304851+Syampk1078@users.noreply.github.com>
2024-12-31 17:05:01 +0530

    initial commit

---
+++ src/main/java/com/example/demo/SpringCrudRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrudRepoApplication.class, args);
+	}
+
+}
